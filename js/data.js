f3d08659060fe0ca3b711b6a43635dcb5e864893
Pablo Salvi <salvipablo@gmail.com>
2022-02-09 08:07:24 -0300

    Tercer commit
    Edicion del archivo index por nuevos soportes
    
    Resolve: T#003

--- js/data.js
+++ js/data.js
@@ -1,141 +1,141 @@
-export const supportData = [{
-        "fecha": "31/01/2022",
-        "dia": "Lunes",
-        "hora": "09:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "31/01/2022",
-        "dia": "Lunes",
-        "hora": "10:30",
-        "cliente": "Ricardo Papetti",
-        "descripcion": "Instalacion del software DSPMC ultima version 7."
-    },
-    {
-        "fecha": "31/01/2022",
-        "dia": "Lunes",
-        "hora": "13:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "31/01/2022",
-        "dia": "Lunes",
-        "hora": "15:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "01/02/2022",
-        "dia": "Martes",
-        "hora": "09:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "01/02/2022",
-        "dia": "Martes",
-        "hora": "10:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "01/02/2022",
-        "dia": "Martes",
-        "hora": "13:30",
-        "cliente": "Ricardo Rigo",
-        "descripcion": "Instalacion del software DSPMC ultima version 8."
-    },
-    {
-        "fecha": "01/02/2022",
-        "dia": "Martes",
-        "hora": "15:00",
-        "cliente": "Jesus Rolando Torres",
-        "descripcion": "Instalacion del software DSPMC ultima version 8."
-    },
-    {
-        "fecha": "02/02/2022",
-        "dia": "Miércoles",
-        "hora": "09:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "02/02/2022",
-        "dia": "Miércoles",
-        "hora": "10:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "02/02/2022",
-        "dia": "Miércoles",
-        "hora": "13:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "02/02/2022",
-        "dia": "Miércoles",
-        "hora": "15:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "03/02/2022",
-        "dia": "Jueves",
-        "hora": "09:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "03/02/2022",
-        "dia": "Jueves",
-        "hora": "10:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "03/02/2022",
-        "dia": "Jueves",
-        "hora": "13:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "03/02/2022",
-        "dia": "Jueves",
-        "hora": "15:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "04/02/2022",
-        "dia": "Viernes",
-        "hora": "09:00",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "04/02/2022",
-        "dia": "Viernes",
-        "hora": "10:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "04/02/2022",
-        "dia": "Viernes",
-        "hora": "13:30",
-        "cliente": "-",
-        "descripcion": "-"
-    },
-    {
-        "fecha": "04/02/2022",
-        "dia": "Viernes",
-        "hora": "15:00",
-        "cliente": "-",
-        "descripcion": "-"
-    }
-];
+// const supportData = [{
+//         "fecha": "07/02/2022",
+//         "dia": "Lunes",
+//         "hora": "09:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "07/02/2022",
+//         "dia": "Lunes",
+//         "hora": "10:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "07/02/2022",
+//         "dia": "Lunes",
+//         "hora": "13:30",
+//         "cliente": "William",
+//         "descripcion": "Instalacion del software DSPMC."
+//     },
+//     {
+//         "fecha": "07/02/2022",
+//         "dia": "Lunes",
+//         "hora": "15:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "08/02/2022",
+//         "dia": "Martes",
+//         "hora": "09:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "08/02/2022",
+//         "dia": "Martes",
+//         "hora": "10:30",
+//         "cliente": "Jonas William",
+//         "descripcion": "Backup de las bases de datos."
+//     },
+//     {
+//         "fecha": "08/02/2022",
+//         "dia": "Martes",
+//         "hora": "13:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "08/02/2022",
+//         "dia": "Martes",
+//         "hora": "15:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "09/02/2022",
+//         "dia": "Miércoles",
+//         "hora": "09:00",
+//         "cliente": "Jorge - Chile",
+//         "descripcion": "Actualizacion a version 8."
+//     },
+//     {
+//         "fecha": "09/02/2022",
+//         "dia": "Miércoles",
+//         "hora": "10:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "09/02/2022",
+//         "dia": "Miércoles",
+//         "hora": "13:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "09/02/2022",
+//         "dia": "Miércoles",
+//         "hora": "15:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "10/02/2022",
+//         "dia": "Jueves",
+//         "hora": "09:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "10/02/2022",
+//         "dia": "Jueves",
+//         "hora": "10:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "10/02/2022",
+//         "dia": "Jueves",
+//         "hora": "13:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "10/02/2022",
+//         "dia": "Jueves",
+//         "hora": "15:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "11/02/2022",
+//         "dia": "Viernes",
+//         "hora": "09:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "11/02/2022",
+//         "dia": "Viernes",
+//         "hora": "10:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "11/02/2022",
+//         "dia": "Viernes",
+//         "hora": "13:30",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     },
+//     {
+//         "fecha": "11/02/2022",
+//         "dia": "Viernes",
+//         "hora": "15:00",
+//         "cliente": "-",
+//         "descripcion": "-"
+//     }
+// ];
